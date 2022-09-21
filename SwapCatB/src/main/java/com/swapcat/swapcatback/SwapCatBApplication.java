19ec6c8a1d3259e208fd6728997001560e572b55
Carlos Cueva <carloswow8@gmail.com>
2022-09-21 10:49:30 +0200

    Create Spring Proyect

---
+++ SwapCatB/src/main/java/com/swapcat/swapcatback/SwapCatBApplication.java
@@ -0,0 +1,13 @@
+package com.swapcat.swapcatback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapCatBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwapCatBApplication.class, args);
+    }
+
+}
---
+++ SwapCatB/src/test/java/com/swapcat/swapcatback/SwapCatBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapcat.swapcatback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapCatBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
